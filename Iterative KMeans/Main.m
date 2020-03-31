cb67a8f91498a937ebc218f74cf330a0d3584e6b
Sedat Kilic <36122776+kilicsedat@users.noreply.github.com>
2020-03-31 13:19:08 -0400

    Update Main.m

--- Iterative KMeans/Main.m
+++ Iterative KMeans/Main.m
@@ -1,6 +1,5 @@
 function [SLL] = Main(filename, k, numofexec, n)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+
 img = imread(filename);
 X = reshape(img, size(img,1)*size(img,2), 3);
 X = double(X);
