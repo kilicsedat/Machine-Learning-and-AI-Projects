40c4809b2ec8fb8cdbdaf4b1d1b97e108844d7ba
Sedat Kilic <36122776+kilicsedat@users.noreply.github.com>
2020-04-10 20:10:56 -0400

    Update get_curvature.m

--- Iris Recognition/get_curvature.m
+++ Iris Recognition/get_curvature.m
@@ -1,8 +1,11 @@
 function curvature = get_curvature(phi,idx)
+    
     [dimy, dimx] = size(phi);        
     [y x] = ind2sub([dimy,dimx],idx);  % get subscripts
+    
     %-- get subscripts of neighbors
     ym1 = y-1; xm1 = x-1; yp1 = y+1; xp1 = x+1;
+    
     %-- bounds checking  
     ym1(ym1<1) = 1; xm1(xm1<1) = 1;              
     yp1(yp1>dimy)=dimy; xp1(xp1>dimx) = dimx;    
